b8c6326519196a0113b60b8e37bbefb41c1a911a
Grace Abike <mara234216@gmail.com>
2022-11-04 17:47:05 +0100

    Add displayed likes-count from API

--- src/utils.js
+++ src/utils.js
@@ -1,59 +1,58 @@
-import mealsURL from './API-utils.js';
+import { getMeals, getLikes, setLikes } from './API-utils.js';
 import displayPopup from './popup.js';
 
-const setMealsCount = async (meals) => {
-  const countElement = document.querySelector('#meals-count');
-  const counter = await meals.length;
-  countElement.innerHTML = `Number of Meals: (${counter})`;
-  return counter;
+const setMealsCount = async (count) => {
+  const mealsCount = await document.querySelector('#meals-count');
+  mealsCount.innerHTML = await `${count} Meals`;
+  return mealsCount;
 };
 
 const displayMeals = async () => {
   try {
-    const fetchedMeals = await fetch(mealsURL);
-    const { meals } = await fetchedMeals.json();
-    setMealsCount(meals);
+    const { meals } = await getMeals();
+    const mealsLikes = await getLikes();
+    setMealsCount(meals.length);
     const pageContent = document.querySelector('#page-content');
     meals.map((meal) => {
       const {
         idMeal, strMeal, strMealThumb,
       } = meal;
+      const likes = mealsLikes.filter((like) => like.item_id === idMeal);
       const mealCard = document.createElement('div');
       mealCard.classList.add('card-section');
       mealCard.innerHTML = `
       <div class="card" meal-id="${idMeal}">
-          <div class="image-content">
-              <span class="overlay"></span>
-              <div class="card-image">
-                  <img id="card-img" class="card-img" src="${strMealThumb}" alt="${strMeal} Image">
-              </div>
+        <div class="image-content">
+          <span class="overlay"></span>
+          <div class="card-image">
+            <img id="card-img" class="card-img" src="${strMealThumb}" alt="${strMeal} Image">
           </div>
-          <div class="card-content">
-              <div class="title">
-                  <div class="name">
-                      <h2 class="meal-name">${strMeal}</h2>
-                  </div> 
-                  <div class="like-content">
-                      <ion-icon id="heart-icon" class="heart-icon" name="heart"></ion-icon>
-                      <span id="like-count" class="like-count"></span>
-                  </div>
-              </div>
-              <div class="card-buttons">
-                  <button id="comment-btn" class="card-btn">Comments</button>
-                  <button id="reservation-btn" class="card-btn">Reservations</button>
-              </div>
+        </div>
+        <div class="card-content">
+          <div class="title">
+            <div class="name">
+              <h2 class="meal-name">${strMeal}</h2>
+            </div> 
+            <div class="like-content">
+              <ion-icon id="heart-icon" class="heart-icon" name="heart"></ion-icon>
+              <span id="like-count" class="like-count">${likes.length > 0 ? likes[0].likes : 0} Likes</span>
+            </div>
           </div>
-      </div>
-      `;
-
+          <div class="card-buttons">
+            <button id="comment-btn" class="card-btn">Comments</button>
+            <button id="reservation-btn" class="card-btn">Reservations</button>
+          </div>
+        </div>
+      </div>`;
       mealCard.querySelector('#comment-btn').addEventListener('click', () => {
         displayPopup(meal);
       });
-
       mealCard.querySelector('#reservation-btn').addEventListener('click', () => {
       });
-
-      mealCard.querySelector('#heart-icon').addEventListener('click', () => {
+      mealCard.querySelector('#heart-icon').addEventListener('click', (e) => {
+        const likeCount = e.target.parentElement.querySelector('#like-count');
+        setLikes(idMeal);
+        likeCount.innerHTML = `${likes[0].likes + 1} Likes`;
       });
 
       return pageContent.appendChild(mealCard);
