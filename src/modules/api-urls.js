09eb0cee32f49707b1400ea4fac172af093e33c6
Grace Abike <mara234216@gmail.com>
2022-11-05 09:29:51 +0100

    Create api-urls.js

---
+++ src/modules/api-urls.js
@@ -0,0 +1,4 @@
+const APImealsURL = 'https://www.themealdb.com/api/json/v1/1/filter.php?a=American';
+const involvementURL = 'https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/fZEB2tpIC45iiUMRjYli/';
+
+export { APImealsURL, involvementURL };
